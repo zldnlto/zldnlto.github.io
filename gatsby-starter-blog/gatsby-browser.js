5adf9abc3727fa333d4670627047982c06e69e55
zldnlto <langchomi0@gmail.com>
2024-04-29 17:59:42 +0900

    Updating Font -IBM Plex Sans KR

--- gatsby-starter-blog/gatsby-browser.js
+++ gatsby-starter-blog/gatsby-browser.js
@@ -1,6 +1,7 @@
 // custom typefaces
 import "@fontsource-variable/montserrat"
 import "@fontsource/merriweather"
+import "@fontsource/ibm-plex-sans-kr"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
